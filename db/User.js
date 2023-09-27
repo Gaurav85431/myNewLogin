c617c7bcb66736d7b3e53a96f82887edd5053267
Gaurav <pushpamgaurav@gmail.com>
2023-09-27 13:01:38 +0530

    Signup

---
+++ db/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+  },
+  mobile: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: Number,
+    required: true
+  }
+
+
+});
+
+module.exports = mongoose.model('User', userSchema);
---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+require('./db/config');
+
+const User = require('./db/User');
+const app = express();
+app.use(express.json());
+
+//signup
+app.post('/signup', async (req, res) => {
+
+  let user = new User(req.body);
+  let result = await user.save();
+  result = result.toObject();
+  delete result.password;
+  res.send(result);
+
+});
+
+// login
+app.post('/login', async (req, res) => {
+
+  if (req.body.email && req.body.password) {
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+
+      res.send(user);
+    }
+    else {
+      res.send({ result: "Not Authorized User" });
+    }
+  }
+  else {
+    res.send({ result: "Not  Authorized User" });
+  }
+
+});
+
+
+app.listen(4000, function () {
+  console.log("Server started");
+});
